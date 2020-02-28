f7413923a08199a3827d6b647086b8216a6a4907
ldaws003 <ldaws003@fiu.edu>
2020-02-27 21:11:42 -0800

    created the main.php file for plugin and added the uninstall.php file

---
+++ user-signup-and-profile-plugin-main.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: User Signup and Profile
+Plugin URI:
+Description: Allows for a custom user to be created for a website where they can only
+make edits to their own profile
+Version: 0.0
+Author:
+License: GNU GENERAL PUBLIC LICENSE
+*/
+
+
+
+?>
