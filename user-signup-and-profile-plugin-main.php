318b2496ac8cca7c3ab25eea302e0f2d5282ee77
ldaws003 <ldaws003@fiu.edu>
2020-02-28 12:01:57 -0800

    created a custom user for businesses

--- user-signup-and-profile-plugin-main.php
+++ user-signup-and-profile-plugin-main.php
@@ -9,6 +9,15 @@ Author:
 License: GNU GENERAL PUBLIC LICENSE
 */
 
-
+// Create a custom user for business
+$result = add_role( 'business', __( 'Business' ),
+                    array( 
+                     'read' => true,
+                     'edit_posts' => false,
+                     'edit_pages' => true,
+                     'edit_other_posts' => false,
+                     'create_posts' => false,
+                     'manage_categores' => false,
+                     'publish_posts' => false                    ) );
 
 ?>
